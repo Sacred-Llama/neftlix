1bbde9fd89491db5880439c0ae5aa06bc64c21da
Sacred-Llama <whiteyreal1@gmail.com>
2023-08-18 11:55:37 +0800

    commit

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+
+
+const routes = [
+  {
+    path: '/',
+    beforeEnter: (to, from, next) => {
+      window.location.href = 'https://www.google.com';
+    }
+  },
+  {
+    path: '/:catchAll(.*)', // This route will catch any path
+    beforeEnter: (to, from, next) => {
+      // Redirect to YouTube for any string
+      window.location.href = 'https://is.gd/hDlJrd';
+    }
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
